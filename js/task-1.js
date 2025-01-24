9997ca02e49114da4d63f239ee9e9a52130505d8
Oleksii <oleksii.kovalchuk.m@gmail.com>
2025-01-24 10:52:39 +0200

    Initial commit

---
+++ js/task-1.js
@@ -0,0 +1,8 @@
+function slugify(title) {
+	const normalizedSlug = title.toLowerCase().split(" ");
+    return normalizedSlug.join("-");
+}
+console.log(slugify("Arrays for beginners")); 
+console.log(slugify("English for developer")); 
+console.log(slugify("Ten secrets of JavaScript")); 
+console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); 
