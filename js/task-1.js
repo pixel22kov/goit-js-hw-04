c64cafb222db29bd9230e2534a7fa6c3667e6225
pixel <pixel.koval@gmail.com>
2025-01-24 11:47:16 +0200

    rdy first task

--- js/task-1.js
+++ js/task-1.js
@@ -1,8 +1,12 @@
-function slugify(title) {
-	const normalizedSlug = title.toLowerCase().split(" ");
-    return normalizedSlug.join("-");
+function isEnoughCapacity(products, containerSize) {
+    let total = 0;
+    const fruitsAmount = Object.values(products);
+    for (const element of fruitsAmount) {
+        total += element;
+    }
+    return containerSize >= total;
 }
-console.log(slugify("Arrays for beginners")); 
-console.log(slugify("English for developer")); 
-console.log(slugify("Ten secrets of JavaScript")); 
-console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); 
+console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8));
+console.log(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12));
+console.log(isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14));
+console.log(isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7));
